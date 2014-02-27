20ed189cde40ca9a31ea8f1432ae8210b6284abd
SupahNickie <nicholascase@live.com>
2014-02-27 07:24:21 -0800

    Added a factorial method and shortest word method

--- cracking_the_coding_interview.rb
+++ cracking_the_coding_interview.rb
@@ -1,3 +1,25 @@
+# ADDING OTHER RANDOM METHODS I THOUGHT TO TRY
+
+def factorial(n)
+  (1..n).inject { |product, n| product * n }
+end
+
+puts factorial(1)
+puts factorial(2)
+puts factorial(3)
+puts factorial(4)
+puts factorial(5)
+puts factorial(6)
+puts factorial(7)
+
+def shortest_word(array)
+  array.inject { |memo, word| word.length < memo.length ? word : memo }
+end
+
+array = ["socks", "cinema", "stare", "cemian", "cokss", "brigade", "tares", "iceman", "time", "rbiagde", "rates", "maniec"]
+
+puts shortest_word(array)
+
 # SCREENING INTERVIEW QUESTION, FLATTEN ARBITRARILY NESTED ARRAYS
 
 def flatten(array)
