b16b63c54dd5ee52bc2080fbc4f3e0b2556838be
SupahNickie <nicholascase@live.com>
2014-03-21 08:34:00 -0700

    Fixed binary depth method

--- cracking_the_coding_interview.rb
+++ cracking_the_coding_interview.rb
@@ -489,7 +489,7 @@ puts "#{leaf5.parent} is leaf5's new parent, which is the 2nd leaf as well"
 
 # BINARY TREE TIME!
 
-class Tree
+class BinaryTree
   attr_accessor :data, :left, :right
   def initialize(data, left = nil, right = nil)
     @data = data
@@ -497,63 +497,75 @@ class Tree
     @right = right
   end
 
+  def child_nodes
+    [@left, @right].compact
+  end
+
   def balanced?
     hash = Hash.new
     hash.default = 0
     self.check_side(hash)
-    hash[:left] > hash[:right] ? "The left side has more branches than the right" : 
-    hash[:right] > hash[:left] ? "The right side has more branches than the left" : 
+    hash[:left] > hash[:right] ? "The left side has more branches than the right" :
+    hash[:right] > hash[:left] ? "The right side has more branches than the left" :
     "The tree has the same amount of left and right branches"
   end
 
   def check_side(hash)
-    if self.left && self.right
+    if @left && @right
       hash[:left] += 1
       hash[:right] += 1
-      self.left.check_side(hash)
-      self.right.check_side(hash)
-    elsif self.left && self.right.nil?
+      @left.check_side(hash)
+      @right.check_side(hash)
+    elsif @left && @right.nil?
       hash[:left] += 1
-      self.left.check_side(hash)
-    elsif self.right && self.left.nil?
+      @left.check_side(hash)
+    elsif @right && @left.nil?
       hash[:right] += 1
-      self.right.check_side(hash)
+      @right.check_side(hash)
     end
   end
 
   def same_depth?
     hash = Hash.new
     hash.default = 0
-    self.left.check_depth(:left, hash)
-    self.right.check_depth(:right, hash)
-    hash[:left] > hash[:right] ? "The left side has more levels of depth than the right" :
-    hash[:right] > hash[:left] ? "The right side has more levels of depth than the left" :
-    "The tree has the same level of depth on the left and right side"
+    if @left && @right
+      hash[:left] = @left.height + 1
+      hash[:right] = @right.height + 1
+      hash[:left] > hash[:right] ? "The left side has more levels of depth (#{hash[:left]}) than the right (#{hash[:right]})" :
+      hash[:right] > hash[:left] ? "The right side has more levels of depth (#{hash[:right]}) than the left (#{hash[:left]})" :
+      "The tree has the same level of depth on the left and right side: #{hash[:right]}"
+    elsif @left && @right.nil?
+      "#{self.height} is the height of the left side with no right"
+    elsif @right && @left.nil?
+      "#{self.height} is the height of the right side with no left"
+    else
+      "There's only one node; the root"
+    end
   end
 
-  def check_depth(side, hash)
-    if self.left || self.right
-      hash[side] += 1
-      self.left.check_depth(side, hash) unless self.left.nil?
-      self.right.check_depth(side, hash) unless self.right.nil?
-    end
+  def height
+    return 0 if self.child_nodes.empty?
+    self.child_nodes.collect {|n| n.height + 1}.max
   end
 end
 
-tree = Tree.new(17, 
-         Tree.new(12, 
-           Tree.new(6, 
-             Tree.new(3), Tree.new(8)), 
-           Tree.new(14, 
-             nil, Tree.new(13))), 
-           Tree.new(28, 
-             Tree.new(23, 
-               Tree.new(18), Tree.new(25)), 
-             Tree.new(34, 
-               Tree.new(29,
-                 Tree.new(28), nil), 
-               Tree.new(30)))
+tree = BinaryTree.new(17,
+         BinaryTree.new(12,
+           BinaryTree.new(6,
+             BinaryTree.new(3), BinaryTree.new(8)),
+           BinaryTree.new(14,
+             nil, BinaryTree.new(13))),
+         BinaryTree.new(28,
+           BinaryTree.new(23,
+             BinaryTree.new(18), BinaryTree.new(25)),
+           BinaryTree.new(34,
+             BinaryTree.new(29,
+               BinaryTree.new(28,
+                BinaryTree.new(11), BinaryTree.new(17)),
+               nil),
+             BinaryTree.new(30)))
         )
+
 # The tree is deeper on the right side, yet has the same amount of left and right "branches"
 puts tree.same_depth?
 puts tree.balanced?
