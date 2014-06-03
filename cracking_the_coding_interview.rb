3edd4ee2f804ba8143cdebd0a87789fbe2006f43
SupahNickie <nicholascase@live.com>
2014-06-03 15:29:02 -0700

    Moved things around, added queue implementations

--- cracking_the_coding_interview.rb
+++ cracking_the_coding_interview.rb
@@ -204,6 +204,171 @@ puts "GETTING RID OF DATA"
 list.remove_duplicates
 list.list_stores
 
+# STACKS
+
+class ArrayStack
+  attr_accessor :top, :values
+  def initialize(top, values = [])
+    @top = top
+    @values = values
+    @values << @top
+  end
+
+  def push(value)
+    @top = value
+    @values << value
+  end
+
+  def pop
+    old_top = @top
+    @values.delete_at(-1)
+    @top = @values.last
+    old_top
+  end
+
+end
+
+
+stack = ArrayStack.new(4)
+print "#{stack.values} is the stack's first set of values\n"
+stack.add(5)
+print "#{stack.values} is the stack's second set of values\n"
+print "#{stack.top} should return 5\n"
+stack.add(6)
+print "#{stack.values} is the stack's third set of values\n"
+print "#{stack.top} should return 6\n"
+print "#{stack.pop} should return the number 6 and delete it afterward\n"
+print "#{stack.values} should show 4 and 5\n"
+print "#{stack.top} should now return the number 5\n"
+
+class LinkedListStack
+  attr_accessor :top
+  def push(value)
+    if @top
+      old_top = @top
+      @top.pointer_ahead = value
+      @top = value
+      @top.pointer_behind = old_top
+    else
+      @top = value
+    end
+  end
+
+  def pop
+    old_top = @top
+    @top = old_top.pointer_behind
+    @top.pointer_ahead = nil; old_top.pointer_ahead = nil; old_top.pointer_behind = nil
+    old_top.data
+  end
+
+  def show_top
+    @top.data
+  end
+end
+
+class Value
+  attr_accessor :data, :pointer_ahead, :pointer_behind
+  def initialize(data, pointer_ahead = nil, pointer_behind = nil)
+    @data = data
+    @pointer_ahead = pointer_ahead
+    @pointer_behind = pointer_behind
+  end
+end
+
+value1 = Value.new(1)
+value2 = Value.new(2)
+stack = LinkedListStack.new
+stack.push(value1)
+stack.push(value2)
+print "#{stack.pop} should return the number 2 and remove it from the stack\n"
+print "#{stack.show_top} should return the number 1 non-destructively\n"
+
+# QUEUES
+
+class ArrayQueue
+  attr_accessor :front, :values
+  def initialize(values = [])
+    @values = values
+  end
+
+  def enqueue(value)
+    @values << value
+    @front = value if @front.nil?
+  end
+
+  def dequeue
+    @values.delete_at(0)
+  end
+
+  def queue_size
+    @values.size
+  end
+
+end
+
+queue = ArrayQueue.new
+queue.enqueue(4)
+queue.enqueue(5)
+queue.enqueue(6)
+print "#{queue.values}\n"
+print "#{queue.front}\n"
+print "#{queue.dequeue} should return and destroy 4\n"
+print "#{queue.queue_size} should return 2\n"
+print "#{queue.values} should show 5 and 6\n"
+
+class LinkedListQueue
+  attr_accessor :front
+  def enqueue(value)
+    if @front
+      head = @front
+      head = head.pointer_behind until head.pointer_behind.nil?
+      head.pointer_behind = value
+      value.pointer_ahead = head
+    else
+      @front = value
+    end
+  end
+
+  def dequeue
+    old_front = @front
+    @front = old_front.pointer_behind
+    @front.pointer_ahead = nil; old_front.pointer_ahead = nil; old_front.pointer_behind = nil
+    old_front.data
+  end
+
+  def queue_size
+    counter = 1
+    head = @front
+    until head.pointer_behind.nil?
+      head = head.pointer_behind
+      counter += 1
+    end
+    counter
+  end
+end
+
+class Value
+  attr_accessor :data, :pointer_ahead, :pointer_behind
+  def initialize(data, pointer_ahead = nil, pointer_behind = nil)
+    @data = data
+    @pointer_ahead = pointer_ahead
+    @pointer_behind = pointer_behind
+  end
+end
+
+value1 = Value.new(1)
+value2 = Value.new(2)
+value3 = Value.new(3)
+queue = LinkedListQueue.new
+queue.enqueue(value1)
+queue.enqueue(value2)
+queue.enqueue(value3)
+print "#{queue.queue_size}\n"
+print "#{queue.dequeue} should return 1 and delete it\n"
+print "#{queue.queue_size} should return 2 after deleting 1\n"
+print "#{queue.dequeue} should now return 2 and delete it\n"
+print "#{queue.queue_size} should return 1 after deleting 2\n"
+
 # TREES
 
 class Tree
